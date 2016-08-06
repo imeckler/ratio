f430a7d3d9b526940dd00e544e402321e01b92cb
John Watson <john.watson@gmx.co.uk>
2016-08-06 14:52:49 +0100

    Export the Rational type constructor
    The constructor is renamed from Ration  to Rational and exported.
    (also add .gitignore)

--- Ratio.elm
+++ Ratio.elm
@@ -5,7 +5,7 @@ module Ratio
         , multiply
         , divide
         , negate
-        , Rational
+        , Rational (..)
         , over
         , denominator
         , numerator
@@ -42,39 +42,39 @@ import Basics exposing (..)
     given bar so that an integer number of copies of it glues together to
     make another given bar.
 -}
-type Rational = Ratio Int Int
+type Rational = Rational Int Int
 
 {-| The biggest number that divides both arguments (the greatest common divisor). -}
 gcd : Int -> Int -> Int
 gcd a b = if b == 0 then a else gcd b (a % b)
 
-normalize (Ratio p q) =
-  let k = gcd p q * (if q < 0 then -1 else 1) in Ratio (p // k) (q // k)
+normalize (Rational p q) =
+  let k = gcd p q * (if q < 0 then -1 else 1) in Rational (p // k) (q // k)
 
 {-| Addition. It's like gluing together two bars of the given lengths. -}
 add : Rational -> Rational -> Rational
-add (Ratio a b) (Ratio c d) =
-  normalize (Ratio (a * d + b * c) (b * d))
+add (Rational a b) (Rational c d) =
+  normalize (Rational (a * d + b * c) (b * d))
 
 {-| Mulitplication. `mulitply x (c / d)` is the length of the bar that you'd get
     if you glued `c` copies of a bar of length `x` end-to-end and then shrunk it
     down enough so that `d` copies of the shrunken bar would fit in the big
     glued bar. -}
 multiply : Rational -> Rational -> Rational
-multiply (Ratio a b) (Ratio c d) =
-  normalize (Ratio (a * c) (b * d))
+multiply (Rational a b) (Rational c d) =
+  normalize (Rational (a * c) (b * d))
 
 {-| Division. It's sort of like multiplication! -}
 divide : Rational -> Rational -> Rational
-divide r (Ratio c d) = multiply r (Ratio d c)
+divide r (Rational c d) = multiply r (Rational d c)
 
 {-| This doesn't really fit with the bar metaphor but this is multiplication by `-1`. -}
 negate : Rational -> Rational
-negate (Ratio a b) = Ratio (-a) b
+negate (Rational a b) = Rational (-a) b
 
 {-| `over x y` is like `x / y`. -}
 over : Int -> Int -> Rational
-over x y = normalize (Ratio x y)
+over x y = normalize (Rational x y)
 
 
 {-| `fromInt x = over x 1` -}
@@ -83,17 +83,17 @@ fromInt x = x `over` 1
 
 {-| -}
 numerator : Rational -> Int
-numerator (Ratio a _) = a
+numerator (Rational a _) = a
 
 {-| -}
 denominator : Rational -> Int
-denominator (Ratio _ b) = b
+denominator (Rational _ b) = b
 
 {-| `split x = (numerator x, denominator x)` -}
 split : Rational -> (Int, Int)
-split (Ratio a b) = (a, b)
+split (Rational a b) = (a, b)
 
 {-| -}
 toFloat : Rational -> Float
-toFloat (Ratio a b) = Basics.toFloat a / Basics.toFloat b
+toFloat (Rational a b) = Basics.toFloat a / Basics.toFloat b
 
